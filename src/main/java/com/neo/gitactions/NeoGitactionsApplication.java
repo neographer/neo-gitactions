49934ee5a362731fa7e7ce3790bef07e1c6e9742
Anoop Kumar <anoopkumar220481@gmail.com>
2021-05-07 21:20:49 +0530

    Sample Spring boot code

---
+++ src/main/java/com/neo/gitactions/NeoGitactionsApplication.java
@@ -0,0 +1,13 @@
+package com.neo.gitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoGitactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoGitactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neo/gitactions/NeoGitactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo.gitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoGitactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
